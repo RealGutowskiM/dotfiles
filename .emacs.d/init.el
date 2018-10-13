58a72bc45b5f3fb5c9092a2fb1228d0b29d448b0
gtwsky <fixzor@hotmail.com>
2018-10-13 20:39:11 +0200

    Add Emacs config
    New Emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,32 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (evil-surround which-key evil-leader evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'evil)
+(evil-mode t)
+
+(require 'evil-leader)
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+
+(require 'evil-surround)
+(global-evil-surround-mode t)
+
+(require 'which-key)
+(which-key-mode)
