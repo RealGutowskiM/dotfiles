7d9f9955de3191a829cfd428ac24f05bc250e2b8
gtwsky <fixzor@hotmail.com>
2018-10-14 18:46:08 +0200

    Modify Emacs config to use Org mode
    Now you config like a boss

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,76 +4,13 @@
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; Disable buttons on top of Emacs
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-
-;; Transparency for terminal
-(defun on-after-init ()
-  (unless (display-graphic-p (selected-frame))
-    (set-face-background 'default "unspecified-bg" (selected-frame))))
-(add-hook 'window-setup-hook 'on-after-init)
-
-;; Relative line numbers
-(global-display-line-numbers-mode t)
-(setq display-line-numbers-type 'relative)
-
-;; Scroll margin
-(setq scroll-margin 10
-      scroll-conservatively 100)
-
-;; Disable annoying bell
-(setq ring-bell-function 'ignore)
-
-;; Line highlighting
-(global-hl-line-mode t)
-
-;; Make symbols pretty
-(global-prettify-symbols-mode t)
-
-;; Disable autosaves + backups
-(setq make-backup-file nil)
-(setq auto-save-default nil)
-
-;; Substitute yes-or-no prompts for shorter y-or-n
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; Disable startup message
-(setq inhibit-startup-message t)
-
 ;; Install use-package for easier package management
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; Packages + their configuration
-(use-package dracula-theme
-             :ensure t
-             :demand
-             :config
-             (load-theme 'dracula t))
-
-(use-package which-key
-             :ensure t
-             :init
-             (which-key-mode))
-
-(use-package beacon
-             :ensure t
-             :init
-             (beacon-mode t))
-
-(use-package evil
-             :ensure t
-             :init
-             (evil-mode t))
-
-(use-package evil-leader
-             :ensure t
-             :init
-             (global-evil-leader-mode t)
-             (evil-leader/set-leader ","))
+;; Load configuration from pretty org file
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -88,4 +25,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "1ASC" :family "Meslo LG S")))))
