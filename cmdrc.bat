7cc0085e7713c804f542a9fe6ba58508a2d5a232
Michał Gutowski <michal.mg.gutowski@gmail.com>
2019-02-20 20:03:03 +0100

    Add Hyper config and update VSCode settings

---
+++ cmdrc.bat
@@ -0,0 +1,12 @@
+@echo off
+
+doskey refreshconfig=%userprofile%\\cmdrc.bat
+
+doskey gs=git status
+doskey gl=git log --graph --oneline --all
+
+doskey pwd=cd
+doskey clear=cls
+doskey ls=dir /O:N
+
+set PROMPT=$T$H$H$H$S-$S$P$_$$$S
