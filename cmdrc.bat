f0d8f3eed69e22dd916ab24b7589fa885bbc0178
Michał Gutowski <michal.mg.gutowski@gmail.com>
2019-03-18 17:55:24 +0100

    Fix console aliases

--- cmdrc.bat
+++ cmdrc.bat
@@ -2,11 +2,12 @@
 
 doskey refreshconfig=%userprofile%\\cmdrc.bat
 
-doskey gs=git status
-doskey gl=git log --graph --oneline --all
+doskey gs=git status $*
+doskey gl=git log --graph --oneline --all $*
 
 doskey pwd=cd
 doskey clear=cls
-doskey ls=dir /O:N
+doskey grep=findstr $*
+doskey ls=dir /O:N $*
 
 set PROMPT=$T$H$H$H$S-$S$P$_$$$S
